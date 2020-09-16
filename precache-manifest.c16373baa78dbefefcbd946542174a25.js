9076d9003adddac759e1884f18bc83bba178ec1d
dimaberkutov <dberkut7@gmail.com>
2020-09-17 01:35:41 +0300

    Updates

---
+++ precache-manifest.c16373baa78dbefefcbd946542174a25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "467ff503be796b2c533f7e503278fbd7",
+    "url": "/react-pagination/index.html"
+  },
+  {
+    "revision": "96459974749177c14044",
+    "url": "/react-pagination/static/css/main.b5403215.chunk.css"
+  },
+  {
+    "revision": "b518175ac9063c7ad85f",
+    "url": "/react-pagination/static/js/2.a215a4f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-pagination/static/js/2.a215a4f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96459974749177c14044",
+    "url": "/react-pagination/static/js/main.7ec954d8.chunk.js"
+  },
+  {
+    "revision": "470abf2cf02bae61db9d",
+    "url": "/react-pagination/static/js/runtime-main.d2d72979.js"
+  }
+]);
